50a8ed6cb5083b2519d58da78b5c86ab8bee76ae
icicel <isakham@chalmers.se>
2023-02-16 03:10:27 +0100

    nil spelling

--- main.lua
+++ main.lua
@@ -98,7 +98,7 @@ MIR.DoorTable = {
 	{{-1, 0}, {0, -1}, {2, 0}, {0, 2}, {-1, 1}, {1, -1}, {1, 1}, {1, 1}}
 }
 MIR.AddWhenOtherVisibleCache = {}
-MIR.LastFloorCacheCleared = null
+MIR.LastFloorCacheCleared = nil
 
 
 
